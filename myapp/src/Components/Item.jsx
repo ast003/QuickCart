e1c3a80ff74d117ddc4df569984b47e862385769
Astha Singh <rks.astha003@gmail.com>
2024-07-03 13:34:17 +0530

    started wroking on cart

--- myapp/src/Components/Item.jsx
+++ myapp/src/Components/Item.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Item = (props) => {
   return (
     <div className='w-60 transform transition-transform duration-300 hover:scale-105'>
-      <Link to={`/product/${props.id}`}><img className='mb-6 'src={props.image}/></Link>
+      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} className='mb-6 'src={props.image}/></Link>
       <p className='text-xs'>{props.name}</p>
       <div className='item-prices text-xs mb-6 font-semibold'>
          ${props.new_price}<span className='line-through text-slate-500 px-2'>${props.old_price}</span>
--- myapp/src/Components/RelatedProducts.jsx
+++ myapp/src/Components/RelatedProducts.jsx
@@ -2,10 +2,10 @@ import data_product from './Assets/data'
 import Item from './Item'
 const RelatedProducts = () => {
   return (
-    <div className='related-products'>
-        <h1>Related Products</h1>
-        <hr/>
-        <div className='related-products-items'>
+    <div className='related-products mt-24'>
+        <h1 className='text-4xl font-semibold flex justify-center mb-4'>Related Products</h1>
+        <hr className='border-t-4 border-black w-1/6 mx-auto mb-12'/>
+        <div className='related-products-items flex gap-x-16 justify-center'>
         {data_product.map((item, i) => {
           return <Item key={i} 
           id={item.id} 
