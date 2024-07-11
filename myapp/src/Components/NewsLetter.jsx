fa745ac25439e5b893218befd6bae2247664f305
Astha Singh <rks.astha003@gmail.com>
2024-07-11 23:19:16 +0530

    Update NewsLetter.jsx

--- myapp/src/Components/NewsLetter.jsx
+++ myapp/src/Components/NewsLetter.jsx
@@ -1,25 +1,4 @@
-// import React from 'react';
 
-// const NewsLetter = () => {
-//   return (
-//     <div className='mt-10 w-3/4  mx-auto p-6 bg-gradient-to-b from-pink-100 to-white text-center'>
-//       <h1 className='text-4xl font-semibold mt-20 mb-8'>Get Exclusive Offers On Your Email</h1>
-//       <p className='text-gray-700-2xl font-medium mb-12'>Subscribe to our Newsletter and stay updated</p>
-//       <div className='flex justify-center items-center'>
-//         <input
-//           type='email'
-//           placeholder='Your E-mail id'
-//           className='p-2 border border-gray-300 rounded w-1/3'
-//         />
-//         <button className='p-2 px-6 bg-gray-950 text-white rounded-full'>
-//           Subscribe
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NewsLetter;
 import React from 'react';
 
 const NewsLetter = () => {
